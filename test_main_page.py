d2ff16f56789e91d36185698fec33d83e1045b09
Mikhail E <evdoshenko.mn@gmail.com>
2020-02-05 12:51:12 +0400

    def test_guest_can_go_to_login_page changes

--- test_main_page.py
+++ test_main_page.py
@@ -5,8 +5,9 @@ def test_guest_can_go_to_login_page(browser):
     url = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, url, timeout=10)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
-    page.should_be_login_link()
-    page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def test_guest_check_login_form_register_form(browser):
     url = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
